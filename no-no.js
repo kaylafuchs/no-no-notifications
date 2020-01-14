28db2ba4eecb830363cb09e2eb589b97c405245f
Kayla Fuchs <kayla.fuchs@vimeo.com>
2020-01-14 14:26:31 -0500

    icons

--- no-no.js
+++ no-no.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (/\(\d+\)/g.test(changeInfo.title)) {
         console.log(`%cHIDING NOTIFICATIONS IN TAB TITLE | ${changeInfo.title}`, 'color: #403BEC; background-color: #A8FFF3; font-weight: bold;');
-        chrome.tabs.executeScript(tab.id, { file: "change-title.js"});
+        chrome.tabs.executeScript(tabId, { file: "change-title.js"});
     }
 });
